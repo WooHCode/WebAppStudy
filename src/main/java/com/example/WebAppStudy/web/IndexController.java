8eee485b23819caff1052dfecf2bae097e8f7985
WooHCode <gusdn526@naver.com>
2022-10-08 00:52:42 +0900

    eighth commit //네이버 로그인 api 구현

--- src/main/java/com/example/WebAppStudy/web/IndexController.java
+++ src/main/java/com/example/WebAppStudy/web/IndexController.java
@@ -24,7 +24,7 @@ public class IndexController {
         model.addAttribute("posts",postsService.findAllDesc());
 
         if(user != null){
-            model.addAttribute("googleUserName", user.getName());
+            model.addAttribute("LoginUserName", user.getName());
         }
         return "index";
     }
